7ba4e48b830f756164fb5cc614eab1f91f1720b3
Gary Royal <nanoonga@proton.me>
2025-04-09 17:40:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,142 @@
+# LiteBox
+
+> An Adaptive Density Graphical Photo Browser written in Computed HTML
+
+![litebox.jpg](litebox.jpg)
+
+## 
+
+## Overview
+
+**LiteBox** is a dpi-aware media browser that tries to render photos with the finest practical image detail on all devices, at all times. It does not require a Super HD video display, but takes full advantage of one if available.
+
+LiteBox is a written in **Computed HTML**, a programming model where the tags describing a web page are assembled in JavaScript and rendered by the browser's HTML interpreter. 
+
+LiteBox introduces **Adaptive Density**, a strategy for optimizing image quality by adjusting the download resolution for each image to match the pixel density of the screen it's being displayed on. 
+
+## Getting Started
+
+* [**View the Live Demo**](https://nanoonga.github.io/litebox/) on your PC, notebook, phone and tablet, or
+
+* Clone or download the repo, and drag the file index.html into an open browser window, then
+
+* Browse the source and adapt what you like to your own projects
+
+## Computed HTML
+
+Computed HTML (CHTML) is a method for developing arbitrarily sophisticated web applications in plain JavaScript, independent of any framework or library. CHTML is orders of magnitude faster than conventional Dynamic HTML (DHTML).
+
+CHTML uses JavaScript to compile a document layout in RAM and passes it to the browser's HTML interpreter as immediate data, rendering the layout in a single pass.
+
+Because all necessary attributes are specified, the browser never has to backtrack or repaint. The user interface is rendered instantly, and becomes interactive in less than a second regardless of layout complexity. 
+
+## Adaptive Density
+
+Super HD (our term for HiDPI video displays like Apple's "Retina") considerably improve the display quality of photos by providing four (or more) hardware pixels per CSS pixel, but only if the software is aware of such pixels and has the means to format images to fill those pixels with detail.
+
+However, [the `srcset` attribute of the HTML`<img>` element](https://www.oxyplug.com/optimization/device-pixel-ratio#serve-image-img-tag-based-on-dpr) is grossly unsuitable for any practical (large, heterogenous, dynamic) media collection because it would involve rendering, storing, and serving a different version of every image for every supported DevicePixelRatio at every presentation size in order for the browser to automagically choose the "best" rendition for the circumstances. 
+
+*Adaptive Density* is an algorithm that holds either the presentation size or the pixel density constant, and computes the resolution necessary to display an image that fulfills the primary constraint. The image may then be downloaded from a server capable of scaling pictures to arbitrary dimensions.
+
+**Mode 1: Constant Density** -- any rendition where the image size >= DevicePixelratio * presentation size, downsampled as necessary by the server. One image pixel per CSS pixel, the best rendition the display is capable of. 
+
+**Mode 0: Constant Area** -- any rendition where the image size < DevicePixelratio * presentation size, upsampled as necessary by the browser. The best rendition the display is capable of *under the circumstances*, which might range from better than Standard HD on a Super HD display to indistinguishable from a Constant Density rendition. 
+
+**Rendition Metrics**
+
+*Adaptive Density Ratio (ADR)* -- image pixels per device pixel (0.88:1, 1.25:2, etc). The shortfall in image resolution expressed as a ratio. The lower the number on the left, the more the image has to be spread to fit.
+
+*Percent Interpolated Pixels (PIP)* -- What percentage of the image is composed of pseudo-pixels interpolated by the browser's image scaler to enlarge the image to presentation size.  
+
+
+
+**Listing 1: Adaptive Density** scales arbitrary images to arbitrary presentation sizes, in this case the height and width of the viewport. This function may be applied to an image whenever the display geometry changes, such as when the window is resized, or the device is rotated. 
+
+```js
+var window_size = [window_width, window_height],
+
+   image_size = [catalog[image_id][WIDTH],catalog[image_id][HEIGHT]],
+
+   aspect_ratio = image_size[WIDTH] / image_size[HEIGHT],    
+
+   render_size = (aspect_ratio >= 1) ? 
+      [ window_size[WIDTH], Math.floor(window_size[WIDTH] / aspect_ratio) ] : 
+      [ Math.floor(window_size[HEIGHT] * aspect_ratio), window_size[HEIGHT] ],
+
+   image_axis = (aspect_ratio >= 1) ? WIDTH : HEIGHT,  
+
+   dpr = devicePixelRatio,  
+
+   render_mode = (image_size[image_axis] >= dpr * render_size[image_axis]) ? 
+'Density' : 'Area',       
+
+  download_size = (render_mode == 'Density') ? 
+        [render_size[WIDTH] * dpr, render_size[HEIGHT] * dpr] : 
+        image_size,
+
+  render_url = `https://picsum.photos/id/${catalog[image_id][ID]}/
+${download_size[WIDTH]}/${download_size[HEIGHT]}`,
+
+   adr = (render_mode=='Density') ? dpr : 
+        (image_size[image_axis] / render_size[image_axis]).toFixed(2),  
+
+   pip = 100-((adr/dpr)*100);   
+```
+
+### notes
+
+- High resolution does not necessarily mean high detail. There are low resolution pictures with high detail, and high resolution pictures with low detail. The origin media (film or digital), source (casual uploads or managed collections), and subject (people, places, artwork) will vary from one picture to the next.
+
+- It may surprise you how little the rendition quality is affected even with a considerable fraction of interpolated pixels. I believe that on Apple devices especially, the image scaler is probably tuned to the display panel, and this is the primary reason that we put the browser in charge of any upscaling to be done rather than downloading an upscaled image from the server. 
+
+- It is a property of raster graphic images that downsampling increases sharpness of definition, so nearly all images will exhibit improved detail simply by being properly scaled for the display panel they're being viewed on. 
+
+---
+
+**Table 1: geometries of a small sample of video displays**
+
+| device                    | resolution | dpr  | ppi             | viewport  |
+| ------------------------- | ---------- | ---- | --------------- | --------- |
+| 27" PC monitor            | 1920x1080  | 1.00 | 82              | 1920x1080 |
+| 9.7" iPad                 | 768x1024   | 1.00 | 132<sup>1</sup> | 768x1024  |
+| 27" iMac 2020             | 2560x1440  | 2.00 | 109<sup>1</sup> | 1280x720  |
+| 12.9" iPad Pro            | 2048x2732  | 2.00 | 264             | 1024x1366 |
+| 3.5" iPhone 4             | 640x960    | 2.00 | 326             | 320x480   |
+| 5.8" Pixel 4a             | 1080x2340  | 2.75 | 443             | 393x851   |
+| 6.1" iPhone 13            | 1170x2532  | 3.00 | 460             | 390x844   |
+| 6.8" Galaxy S23 Ultra     | 1440x3088  | 4.00 | 501             | 360x772   |
+| 14.6" Galaxy Tab S8 Ultra | 1848x2960  | 4.00 | 240             | 462x740   |
+
+<sup>1</sup> It's interesting that a 2011 iPad has a finer dot pitch than a 2020 iMac, despite the iMac being a Super HD ("Retina 5K") display.
+
+---
+
+## Lorem Picsum
+
+**[Lorem Picsum](https://picsum.photos/)** is an image placeholder service that allows us to download arbitrary photos at arbitrary sizes to demonstrate the placement of images in a layout.
+
+Their generous contribution of a scaling image server and photo collection to the public interest made this demonstration possible.
+
+Information about Picsum placeholders is stored in an array called the *catalog*:
+
+```javascript
+var catalog = [
+    // [width, height, picsum ID, author ID, unsplash ID, row]
+    [5000,3333,396,482,"ko-wCySsj-I",0],
+    [4240,2832,667,283,"XMcoTHgNcQA",1],
+    ...
+    [3872,2592,348,40,"mVhd5QVlDWw",892]
+];
+```
+
+*Author ID* = Index of the author's name in the author table for photo credit
+
+*Unsplash ID* = URL path to the photo on [**Unsplash**](https://unsplash.com/about), an archive of free-to-use high-resolution photos
+
+*Row* = the ordinal number of the item in the catalog array
+
+## Wisdom
+
+> *"Anyone can build a fast CPU. The trick is to build a fast system."*
+> 
+> -- Seymour Cray
---
+++ litebox.js
@@ -0,0 +1,486 @@
+/***************************************************************************
+ *
+ *   LiteBox
+ *   An Adaptive Density Graphical Photo Browser written in Computed HTML
+ *   © Copyright Gary Royal 2022, 2025
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ ***************************************************************************/
+
+
+// globals
+
+var 
+    last_width,             
+    columns_per_row, 
+    total_gutter_width, 
+    max_img_width, 
+    render_width, 
+    gallery_width, 
+    left_offset,
+    page_length, 
+    total_pages, 
+    page_number, 
+    column_height, 
+    last_n, 
+    start, 
+    t, 
+    page_length;
+
+
+// settings
+
+const
+    responsive_columns = [0, 0, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 7, 7, 8, 8, 9],
+    gutter_size = 8,
+    alt_max_width = 192,
+    WIDTH = 0, HEIGHT = 1, ID = 2, AUTH = 3, UNSPL = 4, ROW = 5, // pointers into the catalog
+    no = 0, yes = 1,
+    spinner = 'aperture.gif';
+
+
+// preferences
+
+const
+    PAGINATE = yes,
+    DOWNLOAD_LIMIT = 0; // 0 = no limit, else truncate catalog to n = DOWNLOAD_LIMIT photos
+
+if(DOWNLOAD_LIMIT) {
+    catalog = catalog.slice(0,DOWNLOAD_LIMIT-1);
+}
+
+
+
+
+// Computed HTML standard functions
+
+function get_window_geometry() {
+
+    window_width = function() {
+        var x = 0;
+        if (self.innerHeight) {
+            x = self.innerWidth;
+        } else if (document.documentElement && document.documentElement.clientHeight) {
+            x = document.documentElement.clientWidth;
+        } else if (document.body) {
+            x = document.body.clientWidth;
+        }
+        return x;
+    }(),
+
+    window_height = function() {
+        var y = 0;
+        if (self.innerHeight) {
+            y = self.innerHeight;
+        } else if (document.documentElement && document.documentElement.clientHeight) {
+            y = document.documentElement.clientHeight;
+        } else if (document.body) {
+            y = document.body.clientHeight;
+        }
+        return y;
+    }(),
+
+    scrollbar_width = function() {
+        // Creating invisible container
+        const outer = document.createElement('div');
+        outer.style.visibility = 'hidden';
+        outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+        outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+        document.body.appendChild(outer);
+
+        // Creating inner element and placing it in the container
+        const inner = document.createElement('div');
+        outer.appendChild(inner);
+
+        // Calculating difference between container's full width and the child width
+        const scrollbar_width = (outer.offsetWidth - inner.offsetWidth);
+
+        // Removing temporary elements from the DOM
+        outer.parentNode.removeChild(outer);
+
+        return scrollbar_width;
+    }();
+
+    viewport_width = window_width - scrollbar_width;
+}
+
+
+function $(el) {
+
+    // that handy $('foo') shortcut thing
+
+    try {
+        return (typeof el == 'string') ? document.getElementById(el) : el;
+    } catch (e) {
+        if (debug) {
+            alert(el);
+        }
+    }
+}
+
+
+function print_r(obj) {
+
+    // dump an object to the console for debugging
+
+    console.log(JSON.stringify(obj));
+}
+
+
+const
+    svg_paths = {
+        "menu" : '><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>',
+        "info" : '<path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>',
+        "exit" : '<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>'
+    };
+
+    
+function render_icon(name,size,fill) {
+
+    name = name || 'error';
+    size = size || 24;
+    fill = fill || 'currentColor';
+
+    return `<div style="margin:8px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" height="${size}" width="${size}" fill="${fill}">${svg_paths[name]}</svg></div>`;
+}
+
+
+
+
+// lightbox functions
+
+function lightbox_open(image_id) {
+
+    $('lightbox').style.display = 'block';
+    $('menu').style.visibility = 'visible';
+    $('nfobox').style.visibility = 'hidden';    
+
+    // adjust the display position of the nfobox to keep it centered
+
+    $('nfobox').style.top = (window_height-260)/2 + 'px';
+    $('nfobox').style.left = (window_width-260)/2 + 'px';
+
+    // Interesting piece of code:
+    // A nontrivial algorithm that executes in a single declaration statement.
+
+    var window_size = [window_width, window_height],
+
+        image_size = [catalog[image_id][WIDTH],catalog[image_id][HEIGHT]],
+
+        aspect_ratio = image_size[WIDTH] / image_size[HEIGHT],    
+
+        render_size = (aspect_ratio >= 1) ? 
+            [ window_size[WIDTH], Math.floor(window_size[WIDTH] / aspect_ratio) ] : 
+            [ Math.floor(window_size[HEIGHT] * aspect_ratio), window_size[HEIGHT] ],
+
+        image_axis = (aspect_ratio >= 1) ? WIDTH : HEIGHT,  
+
+        dpr = devicePixelRatio,  
+        
+        render_mode = (image_size[image_axis] >= dpr * render_size[image_axis]) ? 'Density' : 'Area',       
+
+        download_size = (render_mode == 'Density') ? [render_size[WIDTH] * dpr, render_size[HEIGHT] * dpr] : 
+        image_size,
+
+        render_url = `https://picsum.photos/id/${catalog[image_id][ID]}/${download_size[WIDTH]}/${download_size[HEIGHT]}`,
+
+        adr = (render_mode == 'Density') ? dpr : (image_size[image_axis] / render_size[image_axis]).toFixed(2),    
+
+        pip = 100-((adr/dpr)*100);
+
+    $('img01').src = render_url;
+
+    $('nfobox').innerHTML = `
+        <table>
+            <tr>
+                <td class="stub">Picsum ID:</td>
+                <td class="col">#&thinsp;${catalog[image_id][ID]}</td>
+            </tr>
+            <tr>
+                <td class="stub">Author:</td>
+                <td class="col"><a href="https://unsplash.com/photos/${catalog[image_id][UNSPL]}" target="_blank">${authors[catalog[image_id][AUTH]]}</a></td>
+            </tr>
+            <tr>
+                <td class="stub">Image:</td>
+                <td class="col">${catalog[image_id][WIDTH]+'&thinsp;x&thinsp;'+catalog[image_id][HEIGHT]}</td>
+            </tr>
+            <tr>
+                <td class="stub">Window:</td><td class="col">${window_size[WIDTH]}&thinsp;x&thinsp;${window_size[HEIGHT]}</td>
+            </tr>
+            <tr>
+                <td class="stub">Render:</td>
+                <td class="col">${render_size[WIDTH] + '&thinsp;x&thinsp;' + render_size[HEIGHT]}</td>
+            </tr>
+            <tr>
+                <td class="stub">Mode:</td>
+                <td class="col">Constant ${render_mode}</td>
+            </tr>
+            <tr>
+                <td class="stub">ADR / PIP:</td>
+                <td class="col">${adr}:${dpr} / ${pip}%</td>
+            </tr>
+
+            <tr>
+                <td class="stub">Download:</td>
+                <td class="col">${download_size[WIDTH] + '&thinsp;x&thinsp;' + download_size[HEIGHT]}</td>
+            </tr>
+        </table>`;
+
+    last_n = image_id;
+}
+
+
+function lightbox_close() {
+
+    nfobox_close();
+    $('menu').style.visibility = 'hidden';
+    $('lightbox').style.display = 'none';    
+    $('img01').src = spinner;
+}
+
+
+function nfobox_toggle() {
+
+    if($('nfobox').style.visibility == 'hidden') {
+        $('nfobox').style.visibility = 'visible';
+    } else {
+        $('nfobox').style.visibility = 'hidden';
+    }
+}
+
+
+function nfobox_close() {
+
+    if($('nfobox').style.visibility == 'visible') {
+        $('nfobox').style.visibility = 'hidden';
+    }
+}
+
+
+
+
+// gallery layout functions
+
+function init() {
+
+    last_width = viewport_width,
+
+    columns_per_row = (viewport_width < 300) ? 1 : ((viewport_width > 2100) ? 12 : responsive_columns[Math.floor(viewport_width / 100)]);
+
+    total_gutter_width = (columns_per_row + 1) * gutter_size,
+
+    max_img_width = (Math.floor((viewport_width - total_gutter_width) / columns_per_row) * 4) / 4,
+
+    render_width = (max_img_width >= alt_max_width) ? alt_max_width : max_img_width,
+
+    gallery_width = (render_width * columns_per_row) + total_gutter_width,
+
+    left_offset = Math.floor((viewport_width - gallery_width) / 2);
+
+    page_length = (PAGINATE) ? Math.ceil(window_height / render_width) * columns_per_row * 2 : catalog.length,
+
+    total_pages = Math.ceil(catalog.length / page_length),
+
+    page_number = 1,
+
+    column_height = new Array(columns_per_row);
+
+    column_height.fill(gutter_size);
+}
+
+
+function fetch_page() {
+
+    // fetch the next page from the catalog
+
+    if(page_number > 0 && page_number <= total_pages) {
+
+        var slice_start = (page_number * page_length) - page_length;
+
+        return catalog.slice(slice_start, slice_start + page_length).map(function(value,index) {
+            return value[ROW];
+        });
+
+    } else {
+        return [];
+    }
+}
+
+
+function auto_paginate() {
+
+    // stream a page of thumbnails to the browser
+
+    if(total_pages > 0) {
+
+        var page = fetch_page();    
+
+        page_length = page.length; 
+
+        if(page_length > 0) {
+
+            var i, // current image
+                j, // current column
+                image_size,
+                aspect_ratio,
+                image_axis,
+                dpr,
+                render_size,
+                download_size,
+                render_url,
+                chtml = [],
+                el;
+
+            // For each photo in the page,
+
+            for(i = 0; i < page_length; i++) {
+
+                // find the column with the shortest height
+
+                j = column_height.indexOf(Math.min(...column_height));
+
+                // scale the image and compile a thumbnail
+
+                image_size = [catalog[page[i]][WIDTH],catalog[page[i]][HEIGHT]],
+
+                aspect_ratio = (image_size[HEIGHT] / image_size[WIDTH]),   
+
+                render_size = [ render_width, Math.floor(aspect_ratio * render_width) ],                
+
+                image_axis = (aspect_ratio >= 1) ? WIDTH : HEIGHT,                  
+
+                dpr = devicePixelRatio,
+
+                render_mode = (image_size[image_axis] >= dpr * render_size[image_axis]) ? 1 : 0,
+
+                download_size = (render_mode) ? [render_size[WIDTH] * dpr, render_size[HEIGHT] * dpr] : render_size,
+
+                render_url = `https://picsum.photos/id/${catalog[page[i]][ID]}/${download_size[WIDTH]}/${download_size[HEIGHT]}`;
+
+                chtml[i] = `<img class="brick" style="top:${ 
+                    column_height[j]
+                }px;left:${
+                    left_offset + gutter_size + (j * (render_size[WIDTH] + gutter_size))
+                }px;width:${
+                    render_size[WIDTH]
+                }px;height:${
+                    render_size[HEIGHT]
+                }px;" src="${ render_url }" loading=lazy onclick="lightbox_open(${
+                    page[i]
+                });">`; // <div class="brick-id"></div>
+
+                // adjust the column height and continue with the next picture
+  
+                column_height[j] += render_size[HEIGHT] + gutter_size;
+            }
+
+            el = document.createElement('div');
+            el.innerHTML = chtml.join('');
+            $('gallery').appendChild(el);
+        }
+    }
+}
+
+
+
+
+// window control functions
+
+function onScroll() {
+
+    if(page_number >= 0) {
+
+        if($('pga').scrollHeight - $('pga').scrollTop - $('pga').clientHeight < 1) {
+
+            page_number++;
+            auto_paginate();
+        }
+    }
+}
+
+
+function debounce(func) {
+
+    // pause execution if window is being resized
+
+    var timer;
+
+    return function(event) {
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(func,100,event);
+    };
+}
+
+
+window.addEventListener("resize",debounce(function(e){
+
+    // re-render gallery after resize or orientation change
+
+    get_window_geometry();
+
+    if (viewport_width != last_width) {
+
+         $('gallery').innerHTML='';
+         init();
+
+        if($('lightbox').style.display == 'block') {
+
+            lightbox_open(last_n);
+        }
+
+        auto_paginate();
+    }
+}));
+
+
+
+
+// And Here. We. Go.
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    get_window_geometry();
+
+    init();
+
+    $('form1').innerHTML = `
+
+    <div id="browser">
+        <div id="pga">
+            <div id="gallery"></div>
+        </div>
+    </div>
+
+    <div id="lightbox">
+        <div id="imgdiv" class="cover" onclick="nfobox_toggle();">
+            <img class="slide" id="img01" src="${ spinner }" onclick="">
+        </div>
+    </div>
+
+    <div id="nfobox" style="top:${(window_height-260)/2}px;left:${(window_width-260)/2}px;" onclick="nfobox_toggle();"></div>
+
+    <nav id="menu" class="menu" style="visibility:hidden;">
+        <span onclick="nfobox_toggle();">${render_icon("info",24)}</span>
+        <span onclick="lightbox_close();">${render_icon("exit",24)}</span>
+    </nav>`;
+
+    if(PAGINATE) {
+
+        // fetch and render the next page on scroll
+        
+        $('pga').addEventListener("scroll", onScroll);
+    }
+
+    // fetch and render the first page
+
+    auto_paginate();
+});
